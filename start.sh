797bb31b88fef3d17df2ff1f05bec74f67e2ec01
miconblog <miconblog@gmail.com>
2014-09-19 16:38:02 +0900

    시작 데몬 수정

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ git reset --hard
 git pull
 bower install
 grunt build
-forever restartall
+forever start -l /app/devicefarm/logs/forever/access.log -o /app/devicefarm/logs/forever/out.log -e /app/devicefarm/logs/forever/err.log -a dist/server/app.js
+node git2distd.js > logs/git2dist.log &
