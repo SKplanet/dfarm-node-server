1e2f43cc7b798ef8e5d4942d414764fe12a729e3
miconblog <miconblog@gmail.com>
2014-10-06 18:26:40 +0900

    노드 모듈 업그레이드

--- server/config/express.js
+++ server/config/express.js
@@ -5,7 +5,7 @@
 'use strict';
 
 var express = require('express');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var morgan = require('morgan');
 var compression = require('compression');
 var bodyParser = require('body-parser');
@@ -29,10 +29,10 @@ module.exports = function(app) {
   app.use(cookieParser());
   app.use(passport.initialize());
   if ('production' === env) {
-    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+    app.use(favicon(path.join(config.root, 'public', 'favicon.ico'), {maxAge:'365d'}));
     app.use(express.static(path.join(config.root, 'public')));
     app.set('appPath', config.root + '/public');
-    app.use(morgan('dev'));
+    app.use(morgan('combined'));
   }
 
   if ('development' === env || 'test' === env) {
@@ -40,7 +40,7 @@ module.exports = function(app) {
     app.use(express.static(path.join(config.root, '.tmp')));
     app.use(express.static(path.join(config.root, 'client')));
     app.set('appPath', 'client');
-    app.use(morgan('dev'));
+    app.use(morgan('common'));
     app.use(errorHandler()); // Error handler - has to be last
   }
 };
