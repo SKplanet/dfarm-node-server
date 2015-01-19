136ec4ee95e09d63dd9daeb0f3c2b2468697fa96
Alan Jeon <skyisle@gmail.com>
2015-01-19 12:45:28 +0900

    Wait for monogo when app start

--- server/app.js
+++ server/app.js
@@ -17,44 +17,47 @@ waitForMongo("mongodb://localhost/comet", {timeout: 1000 * 60* 2}, function(err)
     console.log('timeout exceeded');
   } else {
     console.log('mongodb comes online');
+    run();
   }
 });
 
-// Connect to database
-mongoose.connect(config.mongo.uri, config.mongo.options);
+function run() {
+  // Connect to database
+  mongoose.connect(config.mongo.uri, config.mongo.options);
 
-// Populate DB with sample data
-if(config.seedDB) { require('./config/seed'); }
+  // Populate DB with sample data
+  if(config.seedDB) { require('./config/seed'); }
 
-// Setup server
-var app = express();
-var server = require('http').createServer(app);
-var socketio = require('socket.io')({
-  'browser client gzip': true,
-  'browser client etag': true,
-  'browser client minification': true,
-  'log level': 1,
-  'transports':[
-    'websocket'
-  , 'flashsocket'
-  , 'htmlfile'
-  , 'xhr-polling'
-  , 'jsonp-polling'
-  ]
+  // Setup server
+  var app = express();
+  var server = require('http').createServer(app);
+  var socketio = require('socket.io')({
+    'browser client gzip': true,
+    'browser client etag': true,
+    'browser client minification': true,
+    'log level': 1,
+    'transports':[
+      'websocket'
+    , 'flashsocket'
+    , 'htmlfile'
+    , 'xhr-polling'
+    , 'jsonp-polling'
+    ]
 
-}).listen(server);
+  }).listen(server);
 
-require('./config/socketio')(socketio);
-require('./config/express')(app);
-require('./routes')(app);
+  require('./config/socketio')(socketio);
+  require('./config/express')(app);
+  require('./routes')(app);
 
-// for after db seeding
-setTimeout(adbmon.trackDevice, 1000);
+  // for after db seeding
+  setTimeout(adbmon.trackDevice, 1000);
 
-// Start server
-server.listen(config.port, config.ip, function () {
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
-});
+  // Start server
+  server.listen(config.port, config.ip, function () {
+    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+  });
+  // Expose app
+  exports = module.exports = app;
+}
 
-// Expose app
-exports = module.exports = app;
